140c4e0918a03ec2dcd62c2a2412294bd9397a30
AvadhPansuriya1712 <avadhpansuriya1712@gmail.com>
2025-07-10 22:23:47 +0530

    Change4

--- tests/test_oper.py
+++ tests/test_oper.py
@@ -4,9 +4,11 @@ def test_add():
     assert (2+7)==9
     assert (5+7)==12
     assert (7+3)==10
+    assert (7+9)==16
 
 def test_sub():
     assert (2-5)==-3
     assert (5-1)==4
     assert (3-1)==2
+    assert (9-7)==2
     
